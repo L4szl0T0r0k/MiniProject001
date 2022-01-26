5e093d7714e652adab1659a6aa1aa7b0b61feb6d
L4szl0T0r0k <l4szl0.t0r0k99@gmail.com>
2022-01-27 00:17:23 +0100

    light style add

--- asstets/js/main.js
+++ asstets/js/main.js
@@ -1,1 +1,8 @@
-console.log('Hey')
+console.log('Welcome')
+
+let body = document.querySelector('body');
+let btn = document.querySelector('.btn');
+
+btn.onclick = function() {
+    body.classList.toggle('light')
+}
